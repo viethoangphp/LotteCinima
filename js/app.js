8df59ffa2e29d4ba4cd1f9f2e80c72708a17eb24
VietHoang <hoangcf1134@gmail.com>
2020-06-29 18:04:08 +0700

    add project

---
+++ js/app.js
@@ -0,0 +1,22 @@
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:0,
+  nav:false,
+  autoplay:true,
+  autoplayTimeout:5000,
+  animateOut: 'fadeOut',
+    responsive:{
+      0:{
+        items:1
+      },
+      600:{
+        items:1
+      },
+      1000:{
+        items:1
+      }
+    }
+  })
+$(document).ready(function(){
+    $('.venobox').venobox(); 
+});
